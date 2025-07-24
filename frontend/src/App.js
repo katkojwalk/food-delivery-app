6eaf53874af2da1113c0be490108291f73104862
katkojwal krishna <katkojwalk.7@gmail.com>
2025-07-24 07:05:52 +0530

    fix: switched from VITE_ to REACT_APP_ env for CRA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = import.meta.env.VITE_API_URL + "/api";
+const API_URL = process.env.REACT_APP_API_URL + "/api";
+
 
 function App() {
   const [foodItems, setFoodItems] = useState([]);
